806f0fb986be40518349e75686512e031f0c08aa
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-02-16 22:50:29 +0530

    Footer and Home done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./Pages/Layout/Header"
 import Loading from "./Components/Loading"
+import Footer from "./Pages/Layout/Footer";
 
 const Home = lazy(() => import('./Pages/Home/Home'));
 const Contact = lazy(() => import('./Pages/Contact/Contact'));
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
+          <Footer />
         </Suspense>
       </Router>
     </>
